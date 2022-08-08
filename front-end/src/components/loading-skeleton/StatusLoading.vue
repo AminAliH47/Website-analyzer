f611d39c9f280132bb4547d02fcfa8ed0bfee704
aminali <AminAlih47@gmail.com>
2022-08-08 20:07:42 +0430

    ✅ Skeleton loading added

---
+++ front-end/src/components/loading-skeleton/StatusLoading.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="stat bg-base-100 z-10 animate-pulse">
+        <div class="stat-figure text-primary">
+            <div class="w-12 bg-gray-300 h-12 rounded-full ">
+            </div>
+        </div>
+        <div class="stat-title">
+            <div class="w-28 bg-gray-300 h-3 rounded-md ">
+            </div>
+        </div>
+        <div class="text-primary h-28 flex items-center justify-start">
+            <h2 class="text-4xl font-black">
+                <div class="w-20 bg-gray-300 h-10 rounded-md ">
+                </div>
+            </h2>
+        </div>
+        <div class="stat-desc">
+            <div class="w-32 bg-gray-300 h-3 rounded-md ">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StatusLoadingSkeleton",
+}
+</script>
